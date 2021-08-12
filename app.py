08635a5f311e5f40f227decff5ba541a53915363
Andrei Fokau <andrei.fokau@neutron.kth.se>
2021-08-12 21:53:04 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from wsgiref import simple_server
+
+
+def app(environ, start_response):
+    start_response("200 OK", [])
+    return [os.environ["HTTP_X_FORWARDED_FOR"].encode()]
+
+
+simple_server.ServerHandler.server_software = ""
+port = int(os.environ.get("PORT", 8000))
+with simple_server.make_server(host="", port=port, app=app) as httpd:
+    print(f"Serving on port {port}...")
+    httpd.serve_forever()
