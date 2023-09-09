7e2c30c969e86889fd51089a7b12071253d1cc05
Andrei Fokau <andrei@funnel.io>
2023-09-09 17:07:03 +0200

    Some fixes.

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import os
-import sys
 from wsgiref import simple_server
 
 
 def app(environ, start_response):
     start_response("200 OK", [])
-    return [environ["HTTP_X_FORWARDED_FOR"].encode()]
+    return [(environ.get("HTTP_X_FORWARDED_FOR") or "?").encode()]
 
 
 simple_server.ServerHandler.server_software = ""
-port = int(os.environ.get("PORT", 8000))
+port = int(os.environ.get("PORT") or 8000)
 with simple_server.make_server(host="", port=port, app=app) as httpd:
     print(f"Serving on port {port}...")
     httpd.serve_forever()
